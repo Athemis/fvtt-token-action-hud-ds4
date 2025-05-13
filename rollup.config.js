eaab38b77110ae71fe54385ad9da9ce2ce040d38
Alexander Minges <alexander.minges@gmail.com>
2025-05-13 22:43:25 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,17 @@
-import { terser } from 'rollup-plugin-terser'
-import multi from '@rollup/plugin-multi-entry'
+import terser from "@rollup/plugin-terser";
+import multi from "@rollup/plugin-multi-entry";
 
 export default [
-    {
-        input: {
-            include: [
-                'scripts/*.js',
-                'scripts/*/*.js'
-            ],
-            exclude: [
-                'scripts/token-action-hud-template.min.js']
-        },
-        output: {
-            format: 'esm',
-            file: 'scripts/token-action-hud-template.min.js'
-        },
-        plugins: [
-            terser({ keep_classnames: true, keep_fnames: true }),
-            multi()
-        ]
-    }
-]
+  {
+    input: {
+      include: ["scripts/*.js", "scripts/*/*.js"],
+      exclude: ["scripts/token-action-hud-ds4.min.js"],
+    },
+    output: {
+      format: "esm",
+      file: "scripts/token-action-hud-ds4.min.js",
+      sourcemap: true,
+    },
+    plugins: [terser({ keep_classnames: true, keep_fnames: true }), multi()],
+  },
+];
